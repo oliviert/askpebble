24084635a52036a2b32051e3cce3f2a376a1d2b5
Jauhar <jbasrai@uci.edu>
2014-01-25 02:47:04 -0800

    Update renderQuestion.js

--- server/public/renderQuestion.js
+++ server/public/renderQuestion.js
@@ -1,7 +1,7 @@
 ajax({ url: 'https://raw2.github.com/oliviert/askpebble/master/server/public/question.json' }, function(data) {
 	var data = JSON.parse(data);
 	simply.title('');
-	simply.subtitle(data.question);
+	simply.subtitle("JAUHAR SHABBIR BASRAI");
 	
 	var body = '';
 	for(var choice in data.choices) {
