aec6887454954d4d4609fd6fcce8d3d405d1105a
Eric Walsh <ericmatthewwalsh@gmail.com>
2014-01-25 03:10:05 -0800

    Update testApp.js

--- server/public/testApp.js
+++ server/public/testApp.js
@@ -2,12 +2,13 @@ simply.text({title:'Clickr', subtitle:'The quiz app'}, true);
 
 simply.on('singleClick', function(e) {
 	ajax({ url: 'https://raw2.github.com/oliviert/askpebble/master/server/public/question.json' }, function(data) {
-        var data = JSON.parse(data);
-        simply.subtitle(data.question, true);
-        for(var choice in data.choices) {
-                body += (choice+1) + '. ' + data.choices[choice] + '\n';
-        }
-        simply.body(body);
-        simply.scrollable(true);
-});
+	        var data = JSON.parse(data);
+	        simply.subtitle(data.question, true);
+	        var body = '';
+	        for(var choice in data.choices) {
+	                body += (choice+1) + '. ' + data.choices[choice] + '\n';
+	        }
+	        simply.body(body);
+	        simply.scrollable(true);
+	});
 });
