60029ca3c55692cdc5fc8e249e9fa836f649cd18
Olivier Truong <olivierxtruong@gmail.com>
2014-01-25 02:07:14 -0800

    use mongohq

--- server/main.js
+++ server/main.js
@@ -2,6 +2,16 @@
 
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+
+if (process.env.NODE_ENV == 'production') {
+  app.set('mongo_url', process.env.MONGOHQ_URL);
+} else {
+  app.set('mongo_url', 'mongodb://localhost:27017/askpebble');
+}
+
+mongoose.connect(app.get('mongo_url'));
+
 var Question = require('./models/question');
 
 app.set('port', process.env.PORT || 3000);
@@ -11,11 +21,12 @@ app.use(express.bodyParser());
 app.use(express.static(process.cwd() + '/public'));
 
 app.post('/ask', function(req, res) {
-  res.send('received');
+  var question = new Question({ question: req.body.question });
+  question.save();
 });
 
 app.get('/questions', function(req, res) {
-  res.send('questions');
+  
 });
 
 app.listen(app.get('port'), function() {
