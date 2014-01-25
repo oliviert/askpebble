23f044854baf0ffaf1669abc5438dc32b2c842bf
Jauhar <jbasrai@uci.edu>
2014-01-25 02:47:16 -0800

    Update renderQuestion.js

--- server/public/renderQuestion.js
+++ server/public/renderQuestion.js
@@ -1,7 +1,7 @@
 ajax({ url: 'https://raw2.github.com/oliviert/askpebble/master/server/public/question.json' }, function(data) {
 	var data = JSON.parse(data);
 	simply.title('');
-	simply.subtitle("JAUHAR SHABBIR BASRAI");
+	simply.subtitle("JAUHAR SHABBIR BASRAI the lazy fox jumped over the stupid dog");
 	
 	var body = '';
 	for(var choice in data.choices) {
