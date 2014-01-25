f4d22ee3b3ee1544472a0d7eebbd21c224c228c8
Jauhar <jbasrai@uci.edu>
2014-01-25 02:42:43 -0800

    Update renderQuestion.js

--- server/public/renderQuestion.js
+++ server/public/renderQuestion.js
@@ -5,7 +5,7 @@ ajax({ url: 'https://raw2.github.com/oliviert/askpebble/master/server/public/que
 	
 	var body = '';
 	for(var choice in data.choices) {
-		body += choice;
+		body += choice + '. ' + data.choices[choice] + '\n';
 	}
 	simply.body(body);
 });
