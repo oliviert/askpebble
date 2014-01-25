3c672e5a98ad89b0d2ec3c1a9d15972e307bf894
Eric Walsh <ericmatthewwalsh@gmail.com>
2014-01-25 03:03:16 -0800

    Update renderQuestion.js

--- server/public/renderQuestion.js
+++ server/public/renderQuestion.js
@@ -1,9 +1,11 @@
-simply.text({title:'Clickr', body:'The quiz app'}, true);
-
-simply.on('singleClick', function(e) {
-	if (e.button === 'up') {
-		simply.body('UP you go!');
-	} else if (e.button === 'down') {
-		simply.body('DOWN you go!');
-	}
+ajax({ url: 'https://raw2.github.com/oliviert/askpebble/master/server/public/question.json' }, function(data) {
+        var data = JSON.parse(data);
+        simply.title('');
+        simply.subtitle(data.question);
+        var body = '';
+        for(var choice in data.choices) {
+                body += choice + '. ' + data.choices[choice] + '\n';
+        }
+        simply.body(body);
+        simply.scrollable(true);
 });
